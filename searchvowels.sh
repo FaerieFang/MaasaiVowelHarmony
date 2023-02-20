95a7f9514ac6df6e8a056f4c676287c99a4c4e45
Alexis Schlagenhauf <faerieclique@gmail.com>
2023-02-20 16:04:07 -0700

    now creates list table for vowel co-occorances in seperate file, and prints quanitity to a seperate file correctly

--- searchvowels.sh
+++ searchvowels.sh
@@ -1,3 +1,5 @@
+x=""
+echo "----------------------" >> before_vowels
 for V in a o ɔ e ɛ i ɪ ə u ʊ
 do
 	for S in a o ɔ e ɛ i ɪ ə u ʊ
@@ -5,7 +7,10 @@ do
 		num=$(grep -c "$S[^aoɔeɛiɪəuʊ ]*$V" verbdata)
 		if [ $num -gt 0 ]
 		then
-			echo "vowel $S before $V $num times" > vowelsafter
+			echo "vowel $S before $V $num times" >> beforevowelquantity
+			x="$x $S"
 		fi
 	done
+	echo "'$V'_before '$x'" >> before_vowels
+	x=''
 done
--- searchvowels2.sh
+++ searchvowels2.sh
@@ -1,3 +1,5 @@
+x=""
+echo "----------------------" >> after_vowels
 for V in a o ɔ e ɛ i ɪ ə u ʊ
 do
 	for S in a o ɔ e ɛ i ɪ ə u ʊ
@@ -5,7 +7,10 @@ do
 		num=$(grep -c "$V[^aoɔeɛiɪəuʊ ]*$S" verbdata)
 		if [ $num -gt 0 ]
 		then
-			echo "vowel $S after $V $num times" > vowelsbefore
+			echo "vowel $S after $V $num times" >> vowelafterquantity
+			x="$x $S"
 		fi
 	done
+	echo "'$V'_after '$x'" >> after_vowels
+	x=''
 done
